7b6a56bfb3d54455d6707215882e7abe01b0697b
hugojay <hugochen@yahoo-inc.com>
2013-05-17 18:02:08 +0800

    增三八CSS3效果/增點擊復原掀起
    亂玩

--- blowpaper.js
+++ blowpaper.js
@@ -2,22 +2,40 @@ $(function(){
 	$(".blowpaper").each(function (i, v) {
 		$(this).children("img").before("<div class='paper_corner'><img src='blowpaper_corner@2x.png' width=262 height=61></div>");
 		$(".paper_corner").on("click", "img", function(){
-			$(this).parents(".blowpaper").children(".coupon").show();
-			$(this).parents(".blowpaper").children(".paper_corner").animate({
-					height: 300 // -100
-			}, 200, "linear");
-			$(this).parents(".blowpaper").children("img").animate({
-					width: 400,
-					height: 300 // -100
-			}, 200, "linear");
-			$(this).animate({
+			var status = $(this).data("status") || "closed";
+			if(status == "closed"){
+				$(this).data("status", "opened");
+				$(this).parents(".blowpaper").each(function(){
+					$(this).children("img").animate({
+						width: 400,
+						height: 300 // -100
+					}, 200, "linear");
+					$(this).children(".coupon").stop().fadeIn(1);
+					$(this).children(".paper_corner").animate({
+						height: 300 // -100
+					}, 200, "linear");
+				});
+				$(this).animate({
 					width: 400,
 					height: 140
-			}, 200, "linear", function(){
-				//$(this).fadeOut(20);
-			});
+				}, 200);
+			}else{
+				// reverse
+				$(this).data("status", "closed");
+				$(this).parents(".blowpaper").children("img").animate({
+					width: 400,
+					height: 400
+				}, 300, "linear");
+				$(this).parents(".blowpaper").children(".coupon").fadeOut(500, "linear");
+				$(this).parents(".blowpaper").children(".paper_corner").animate({
+					height: 400
+				}, 300, "linear");
+				$(this).animate({
+					width: 262,
+					height: 61
+				}, 300, "linear");
+			}
 		});
-
 	});
 
 	// shakejs event binding
